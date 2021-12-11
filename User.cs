8f71faa68abba40f98ab326444171b006e1026e9
Casper-NS <37834568+Casper-NS@users.noreply.github.com>
2021-12-12 00:16:14 +0100

    Added validation for usernames and emails
    Still need to add tests.

--- User.cs
+++ User.cs
@@ -13,11 +13,11 @@ namespace Stregsystemet
         {
             ID = id;
 
-            UserName = username;
-            FirstName = firstname;
-            LastName = lastname;
+            UserName = ValidateUserName(username) ? username : throw new ArgumentException("Invalid Username - valid characters: [0-9], [a-z] and '_'");
+            FirstName = string.IsNullOrEmpty(firstname) ? throw new ArgumentNullException("String: First Name can't be null or empty") : firstname;
+            LastName = string.IsNullOrEmpty(lastname) ? throw new ArgumentNullException("String: Last Name can't be null or empty") : lastname;
 
-            Email = email;
+            Email = ValidateEmail(email) ? email : throw new ArgumentException("Invalid Email");
             Balance = initialBalance;
         }
 
@@ -44,12 +44,58 @@ namespace Stregsystemet
 
         private bool ValidateUserName(string username)
         {
-            throw new NotImplementedException();
+            string validCharacters = "abcdefghijklmnopqrstuvwxyz1234567890_";
+
+            foreach(char character in username)
+            {
+                if (!validCharacters.Contains(character))
+                {
+                    return false;
+                }
+            }
+
+            return true;
         }
 
         private bool ValidateEmail(string email)
         {
-            throw new NotImplementedException();
+            string validCharacters = "abcdefghijklmnopqrstuvwxyz1234567890_.-";
+
+            string[] parts;
+            string localPart;
+            string domainPart;
+
+            if (email.Count(c => c == '@') > 1)
+            {
+                parts = email.Split('@');
+                localPart = parts[0];
+                domainPart = parts[1];
+            }
+            else
+            {
+                return false;
+            }
+
+            foreach (char character in localPart)
+            {
+
+                if (!validCharacters.Contains(character))
+                {
+                    return false;
+                }
+            }
+
+            validCharacters = validCharacters.Remove(validCharacters.IndexOf('_'), 1);
+
+            foreach (char character in domainPart)
+            {
+                if (!validCharacters.Contains(character))
+                {
+                    return false;
+                }
+            }
+
+            return true;
         }
     }
 }

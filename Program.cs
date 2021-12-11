a6965b2068d0f1d490ee1b487d4c844569ff5874
Casper-NS <37834568+Casper-NS@users.noreply.github.com>
2021-12-11 21:58:36 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Stregsystemet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //IStregsystem stregsystem = new Stregsystem();
+            //IStregsystemUI ui = new StregsystemCLI(stregsystem);
+            //StregsystemController sc = new StregsystemController(ui, stregsystem);
+            //ui.Start();
+        }
+    }
+
+}
